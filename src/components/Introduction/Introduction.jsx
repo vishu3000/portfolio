34687df6a22ec82bc1c6ad975f1bd400116ca348
vishu3000 <53874372+vishu3000@users.noreply.github.com>
2023-10-14 12:32:11 +0530

    First push

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 import { annotate, annotationGroup } from "rough-notation";
 import styles from "./Introduction.module.scss";
@@ -21,17 +22,18 @@ export const Introduction = () => {
 
   return (
     <div className="container">
+      {/* First Section */}
       <div className="row ">
         <div className="col-7">
           <div className="row gy-5">
             <div className="col-11">
               <p id="dev" className={styles.fontEnhancement}>
-                Developer
+                Developer.
               </p>
             </div>
             <div className="col-11">
               <p id="prog" className={styles.fontEnhancement}>
-                Programmer
+                Programmer.
               </p>
             </div>
           </div>
@@ -45,10 +47,29 @@ export const Introduction = () => {
               alt="avatar"
               className={`rounded ${styles.image}`}
             />
-            <div></div>
+            <div>
+              <div class="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-90deg-up"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.854 1.146a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L4 2.707V12.5A2.5 2.5 0 0 0 6.5 15h8a.5.5 0 0 0 0-1h-8A1.5 1.5 0 0 1 5 12.5V2.707l3.146 3.147a.5.5 0 1 0 .708-.708l-4-4z"
+                  ></path>
+                </svg>
+                <p class="font-mono">That's me</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      {/* Second Section */}
+      <div className={`${styles.backgroundSec}`}></div>
     </div>
   );
 };
